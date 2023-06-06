094a6b33d5a824650eb3ce665fc11442a2870ec0
Fioku <yosry3196@gmail.com>
2023-06-06 14:15:22 +0300

    hahaha

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.read_jobs),
     path('<int:id>', views.job_detail),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
